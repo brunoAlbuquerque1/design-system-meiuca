1ced00d01e3bb714ec517a2de1a4252ef5623dd6
brunoAlbuquerque1 <brunoalbuquerque.258@gmail.com>
2023-05-06 23:03:08 -0300

    chore: init project tokens

---
+++ design-tokens/build/ios/StyleDictionaryColor.h
@@ -0,0 +1,26 @@
+
+//
+// StyleDictionaryColor.h
+//
+
+// Do not edit directly
+// Generated on Sun, 07 May 2023 02:02:00 GMT
+
+
+#import <UIKit/UIKit.h>
+
+typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
+ColorBaseGrayLight,
+ColorBaseGrayMedium,
+ColorBaseGrayDark,
+ColorBaseRed,
+ColorBaseGreen,
+ColorFontBase,
+ColorFontSecondary,
+ColorFontTertiary
+};
+
+@interface StyleDictionaryColor : NSObject
++ (NSArray *)values;
++ (UIColor *)color:(StyleDictionaryColorName)color;
+@end
---
+++ design-tokens/build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Sun, 07 May 2023 02:02:00 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
